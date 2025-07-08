186f2d8afdd94fa717b3099b03b2e7527c396423
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2025-07-08 11:14:35 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,3 @@
-CREATE INDEX IF NOT EXISTS idx_users_ID ON users (user_id);
-CREATE INDEX IF NOT EXISTS idx_bookings_ID ON bookings (booking_id);
-CREATE INDEX IF NOT EXISTS idx_Property_ID ON Properties (Property_id);
+EXPLAIN ANALYZE CREATE INDEX IF NOT EXISTS idx_users_ID ON users (user_id);
+EXPLAIN ANALYZE CREATE INDEX IF NOT EXISTS idx_bookings_ID ON bookings (booking_id);
+EXPLAIN ANALYZE CREATE INDEX IF NOT EXISTS idx_Property_ID ON Properties (Property_id);
